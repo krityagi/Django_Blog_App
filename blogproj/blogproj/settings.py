27254e47be66bf3e6a2620cf1ed02bd2ed527389
ktyagi0602@gmail.com <ktyagi0602@gmail.com>
2023-04-04 01:30:30 +0100

    updated app.conf for static files

--- blogproj/blogproj/settings.py
+++ blogproj/blogproj/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_URL = BASE_DIR / 'blogproj/blog/static'
-STATIC_ROOT = os.path.join(BASE_DIR, '/blogproj/static/')
+STATIC_ROOT = os.path.join(BASE_DIR/'blogproj', 'static/')
 
 STATICFILES_DIR = [
-    BASE_DIR / "blogproj/static",
+    BASE_DIR / "static",
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
