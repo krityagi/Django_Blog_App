97ed713edcb1dac7bc1d660953e6a131a6ca32e7
ktyagi0602@gmail.com <ktyagi0602@gmail.com>
2023-04-06 00:42:20 +0100

    updated app.conf for port 8443

--- blogproj/blog/urls.py
+++ blogproj/blog/urls.py
@@ -6,10 +6,9 @@ from django.views.static import serve
 
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.signin, name="signin"),
+    path('home', views.home, name="home"),
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
     path('signout', views.signout, name="signout"),
-    re_path(r'^static/(?P<path>.*)$', serve,
-            {'document_root': settings.STATIC_ROOT}),
 ]
